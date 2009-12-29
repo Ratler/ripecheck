7ed5cef1b6f539ecf648da92f435e96ff6cb2b22
Stefan Wold <ratler@stderr.eu>
2009-12-29 18:17:28 +0100

    Bumping version to 3.1-rc1

--- ripecheck.tcl
+++ ripecheck.tcl
@@ -1,5 +1,5 @@
 #
-# ripecheck.tcl  Version: 3.0.3  Author: Stefan Wold <ratler@stderr.eu>
+# ripecheck.tcl  Version: 3.1-rc1  Author: Stefan Wold <ratler@stderr.eu>
 ###
 # Info:
 # This script check unresolved ip addresses against a RIPE database
@@ -171,7 +171,7 @@ bind msg -|- !ripeinfo ::ripecheck::msgRipeInfo
 
 namespace eval ::ripecheck {
     # Global variables
-    variable version "3.0.3"
+    variable version "3.1-rc1"
 
     variable maskarray
     variable chanarr
