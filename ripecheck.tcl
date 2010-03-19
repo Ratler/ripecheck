1c0627c6e8ebb0a34aff5d96ff80f23ab9bca6a0
Stefan Wold <ratler@stderr.eu>
2010-03-19 21:58:12 +0100

    Updated GPL license text

--- ripecheck.tcl
+++ ripecheck.tcl
@@ -124,10 +124,10 @@
 #
 # This code comes with ABSOLUTELY NO WARRANTY
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -136,7 +136,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # RIPE Country Checker
 
