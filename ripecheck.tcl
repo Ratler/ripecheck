930729851187faf6888ec9f294f7bcb3ae767e93
Stefan Wold <ratler@stderr.eu>
2010-10-01 14:20:50 +0200

    Version bumped to 3.4-dev, new features added

--- ripecheck.tcl
+++ ripecheck.tcl
@@ -1,5 +1,5 @@
 #
-# ripecheck.tcl  Version: 3.3.3-dev  Author: Stefan Wold <ratler@stderr.eu>
+# ripecheck.tcl  Version: 3.4-dev  Author: Stefan Wold <ratler@stderr.eu>
 ###
 # Info:
 # This script check unresolved ip addresses against a RIPE database
@@ -208,7 +208,7 @@ bind pub -|- !ripetld ::ripecheck::pubRipeTld
 
 namespace eval ::ripecheck {
     # Global variables
-    variable version "3.3.3-dev"
+    variable version "3.4-dev"
 
     variable ipinfodb "http://ipinfodb.com/ip_query.php?ip="
     variable maskarray
